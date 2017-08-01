949456187e00389d85961c51ce119954860e07aa
Cameron Alizadeh <cdalizadeh@gmail.com>
2017-07-31 23:47:23 -0400

    preliminary backend support

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.status(200).send();
+});
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Listening on port 3000');
+});
