025f6f1e492ae3baa8fb5ebdc35316d664b8fa07
Cameron Alizadeh <cdalizadeh@gmail.com>
2017-08-04 16:24:48 -0400

    completed back end

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -8,10 +9,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-    console.log(req.body);
-    res.status(200).send();
+    var body = req.body;
+    if (!body || !body.comments) res.status(400).send();
+    else{
+        var text = 'Date: ' + (new Date().toLocaleString()) + '\n';
+        if (body.name) text += 'Name: ' + body.name + '\n';
+        if (body.email) text += 'Email: ' + body.email + '\n';
+        text += 'Comments: ' + body.comments;
+        sendMail(text, function(err, body){
+            if (err) res.status(500).send(err);
+            else res.json(body);
+        });
+    }
 });
 
 var server = app.listen(app.get('port'), function() {
     console.log('Listening.');
 });
+
+function sendMail(text, cb){
+    var headers = {
+        'Authorization': 'Basic ' + process.env.MAILGUN_KEY
+    };
+    var formData = {
+        from: 'Automated Feedback Mailer<website@blueandgold.mailgun.org>',
+        to: 'cdalizadeh@gmail.com',
+        subject: 'Website Feedback Received',
+        text: text
+    }
+    request({
+        url: 'https://api.mailgun.net/v3/sandbox2967c7328b27470ca8bbbda44c6291da.mailgun.org/messages',
+        method: 'POST',
+        headers: headers,
+        form: formData,
+        callback: function(err, res, body){
+            if (err || JSON.parse(body).message != 'Queued. Thank you.') cb('Internal Mailing Error');
+            else cb(null, body);
+        }
+    });
+}
